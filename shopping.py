20e9a07e18c01062a58ab50668e376747e9ce493
katherinelundberg <baugher.katherine@gmail.com>
2017-01-31 21:14:55 -0800

    Add files via upload

--- shopping.py
+++ shopping.py
@@ -11,8 +11,23 @@ def check_shopping_list(items):
         shopping_list.append(items)
         print "This now added to your list."
     shopping_list.sort()
-    
+
 check_shopping_list(needed_items)
 
 print shopping_list
 
+item_to_remove = raw_input("What item do you want to remove?")
+
+
+def edited_list(items):
+    items = items.lower()
+    if items not in shopping_list:
+        print "Sorry can't remove that."
+    else:
+        shopping_list.remove(items)
+        print "This has been removed."
+    shopping_list.sort()
+
+edited_list(item_to_remove)
+
+print shopping_list
